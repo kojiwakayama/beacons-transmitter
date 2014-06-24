8c2b87532fcf5dd26d0e3129fde381bc214c4d75
kojiwakayama <github@kojiwakayama.com>
2014-06-25 01:41:14 +0200

    Add inputfield for url+port

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,7 +2,6 @@ var io = require('socket.io');
 var socket;
 
 var sendData = function (data) {
-  Ti.API.info('sendData!');
   Ti.API.info(data);
   socket.emit('beacons::create', data, {}, function(error, beacons) {});
 };
@@ -43,21 +42,19 @@ if (OS_IOS) {
   };
 
   var updateRanges = function (e) {
-    e.beacons.forEach(function(device) {
-      var data = {
-        event: 'update-ranges',
-        timestamp: new Date().getTime(),
-        id: device.uuid+"-"+device.major+"-"+device.minor,
-        identifier: device.identifier,
-        uuid: device.uuid,
-        major: parseInt(device.major),
-        minor: parseInt(device.minor),
-        proximity: device.proximity,
-        rssi: device.rssi,
-        distance: device.accuracy
-      };
-      sendData(data);
-    });
+    var data = {
+      event: 'update-ranges',
+      timestamp: new Date().getTime(),
+      id: e.uuid+"-"+e.major+"-"+e.minor,
+      identifier: e.identifier,
+      uuid: e.uuid,
+      major: parseInt(e.major),
+      minor: parseInt(e.minor),
+      proximity: e.proximity,
+      rssi: e.rssi,
+      distance: e.accuracy
+    };
+    sendData(data);
   };
 
   var handleProximity = function (e) {
@@ -107,29 +104,30 @@ if (OS_IOS) {
 
   TiBeacons.enableAutoRanging();
 
+  // Todo: fix socket.io reconnect
   var toggleMonitoring = function () {
+    var urlPort = $.urlPortInput.value;
+
+    if (!urlPort) {
+      $.monitoringSwitch.value = false;
+      return;
+    }
+
     if ($.monitoringSwitch.value) {
-      var uri = 'ws://10.6.112.133:8000';
+      var uri = 'ws://' + $.urlPortInput.value;
 
       socket = io.connect(uri);
-
       socket.on('connect', function () {
-        Ti.API.log('connected!');
-
-        //All dev beacons from Estimote got the same uuid
+        // All dev estimote-beacons have the same uuid
         TiBeacons.startMonitoringForRegion({
           uuid : "B9407F30-F5F8-466E-AFF9-25556B57FE6D",
           identifier : "Estimote"
         });
-
         Ti.API.info('start');
       });
     } else {
-      socket.on('disconnect', function () {
-        TiBeacons.stopMonitoringAllRegions();
-        Ti.API.info('stop');
-      });
-      socket.disconnect();
+      TiBeacons.stopMonitoringAllRegions();
+      Ti.API.info('stop');
     }
   };
 }
@@ -138,8 +136,6 @@ if (OS_ANDROID) {
   var iBeacon = require('miga.tibeacon');
 
   var onSuccess = function (e) {
-    Ti.API.log('onSuccess!');
-
     e.devices.forEach(function(device) {
         var data = {
           event: 'on-success',
@@ -189,21 +185,28 @@ if (OS_ANDROID) {
   };
 
   var onError = function (e) {
-    Ti.API.log('onError!');
     Ti.API.info(JSON.stringify(e));
   };
 
-  // register success Callback and set interval to 10sec
+  // Register success callback and set interval to 10sec
   iBeacon.initBeacon({
       success : onSuccess, error:onError, interval: 10, region: onRegion, found:onFound
   });
 
+  // Todo: fix socket.io reconnect
   var toggleMonitoring = function () {
+    var urlPort = $.urlPortInput.value;
+
+    if (!urlPort) {
+      $.monitoringSwitch.value = false;
+      return;
+    }
+
     if ($.monitoringSwitch.value) {
-      var uri = 'ws://10.6.112.133:8000';
+     var uri = 'ws://' + $.urlPortInput.value;
+
       socket = io.connect(uri);
       socket.on('connect', function () {
-        Ti.API.log('connected!');
         iBeacon.startScanning();
         Ti.API.info('start');
       });
@@ -218,7 +221,6 @@ if (OS_ANDROID) {
 }
 
 var init = function () {
-  Ti.API.log('init!');
   $.win.open();
 };
 
